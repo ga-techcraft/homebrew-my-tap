72fe84844e965553dbca914de5fef5253ef24c14
GaJiye <ykjustin007@gmail.com>
2024-12-28 12:53:22 +0900

    Fix sha256 hash for Git 2.30.0

--- Formula/git@2.30.0.rb
+++ Formula/git@2.30.0.rb
@@ -2,7 +2,7 @@ class GitAT2300 < Formula
   desc "Distributed version control system"
   homepage "https://git-scm.com"
   url "https://github.com/git/git/archive/v2.30.0.tar.gz"
-  sha256 "ad2c90a5b318b4087bb3a8e04a96fd362d06b9ac5ed157d1190fce35a7b5b504"
+  sha256 "8db4edd1a0a74ebf4b78aed3f9e25c8f2a7db3c00b1aaee94d1e9834fae24e61"
 
   depends_on "gettext"
   depends_on "pcre2"
